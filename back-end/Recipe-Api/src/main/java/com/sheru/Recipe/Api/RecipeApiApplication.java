06a9a3e889018d7dd9744469655caedea842bd3d
Shreyans Sethia <sheru.sherki28@gmail.com>
2023-11-17 10:33:14 +0530

    added changes required for tomcat

--- back-end/Recipe-Api/src/main/java/com/sheru/Recipe/Api/RecipeApiApplication.java
+++ back-end/Recipe-Api/src/main/java/com/sheru/Recipe/Api/RecipeApiApplication.java
@@ -2,9 +2,15 @@ package com.sheru.Recipe.Api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RecipeApiApplication {
+public class RecipeApiApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+		return applicationBuilder.sources(RecipeApiApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecipeApiApplication.class, args);
